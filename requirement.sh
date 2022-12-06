ed0758d09e0e30a726888ad17addb69645a6fad5
llaoj <qustwwy@163.com>
2022-12-06 09:47:17 +0800

    requirement check

---
+++ requirement.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# kenerl version check
+
+# rbd module support
+
+# ipvs module support
+
+# internet support
+
+# docker support
+
+# docker-compose support
+
+# ceph-common support
